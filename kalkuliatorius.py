34cfbf3d6c0c87eee0e294c47f45c5401946c4d4
Darius P <darius.pyplys@balticclothing.lt>
2023-10-26 09:50:59 +0300

    kalkuliatoius, prideta sudetis

--- kalkuliatorius.py
+++ kalkuliatorius.py
@@ -16,3 +16,14 @@ while True:
     meniu_pasirinkimas = input(" ")
 
     if meniu_pasirinkimas == "q":
+        break
+
+        if meniu_pasirinkimas not in ("1", "2", "3", "4"):
+            print("Tokio pasirinkimo nėra")
+            continue
+        # naudojam float - skaičius su kableliu
+        sk1 = float(input("Įveskite pirmąjį skaičių "))
+        sk2 = float(input("Įveskite antrąjį skaičių "))
+
+        if meniu_pasirinkimas == "1":
+            res = f"{sk1} + {sk2} = {sk1 + sk2}"
